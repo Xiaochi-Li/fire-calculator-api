4c63607d00203576ceafecceb5cff700f4423210
Xiaochi-Li <seanli@geniesolutions.com.au>
2024-06-16 14:36:17 +1000

    chore: Update app-data-source.ts with Docker host configuration

--- src/app-data-source.ts
+++ src/app-data-source.ts
@@ -7,6 +7,7 @@ export const myDataSource = new DataSource({
   // that are running on the host machine. Docker provides a special DNS name, `host.docker.internal`,
   // which resolves to the internal IP address of the host machine. This allows the application inside
   // the Docker container to access services on the host machine.
+  host: "host.docker.internal",
   // host: "localhost",
   port: 3306,
   username: "fjdlsajfel",
